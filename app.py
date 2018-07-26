e2f8a0478f1e30a4787df02070ee536c6b69ff68
Warren Wrate <bustawhero@outlook.con>
2018-07-26 08:58:31 -0700

    app_working_with_files

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
 
+d = {}
+
+old_d = {}
+
+def splitline(strtext, _dict):
+	key = strtext.split('-')[0] + '-' + strtext.split('-')[1]
+	_dict[key] = strtext
+
 with open('newdata.txt', 'r') as f:
 	for line in f:
-		print(line)
+		splitline(line, d)
+
+with open('olddata.txt', 'r') as o:
+	for line in o:
+		splitline(line, old_d)
+
+count = 0
+
+new_rows = set(d.keys()) - set(old_d.keys())
+changed_rows = set(d.keys()) & set(old_d.keys())
+ended_rows = set(old_d.keys()) - set(d.keys())
+
+print('new rows', new_rows)
+print('ended_rows', ended_rows)
+
