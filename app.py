c3cf9ce8be023008fbcb17428c93f2ff54e0673e
Warren Wrate <bustawhero@outlook.con>
2018-07-26 13:54:07 -0700

    changes_working

--- app.py
+++ app.py
@@ -18,9 +18,24 @@ with open('olddata.txt', 'r') as o:
 count = 0
 
 new_rows = set(d.keys()) - set(old_d.keys())
-changed_rows = set(d.keys()) & set(old_d.keys())
+changed_rows = list(set(d.keys()) & set(old_d.keys()))
 ended_rows = set(old_d.keys()) - set(d.keys())
 
 print('new rows', new_rows)
 print('ended_rows', ended_rows)
+print('new row count', len(new_rows))
 
+for n in new_rows:
+	ptpid, benid, startdate, tier, plan, coverageamount = d[n].split('-')
+	print('ParticipantID:{}, StatDate:{}, Tier:{}, Plan:{}'.format(ptpid, startdate, tier, plan))
+
+for x in changed_rows:
+	ptpid, benid, startdate, tier, plan, coverageamount = d[x].split('-')
+	o_ptpid, o_benid, o_startdate, o_tier, o_plan,  o_coverageamount = old_d[x].split('-')
+	if d[x] != old_d[x]:
+		print('New Values: ParticipantID:{}, StatDate:{}, Tier:{}, Plan:{}'.format(ptpid, startdate, tier, plan))
+		print('Old Values: ParticipantID:{}, StatDate:{}, Tier:{}, Plan:{}'.format(o_ptpid, o_startdate, o_tier, o_plan))
+		count += 1
+		
+
+print('changes:', count)
